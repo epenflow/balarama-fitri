27e0be5dcc3fdd16670aa9bd91dcbbe7169f1770
epenflow <epenflow@gmail.com>
2024-04-28 18:22:08 +0800

    icon

--- src/app/components/Galleri.tsx
+++ src/app/components/Galleri.tsx
@@ -1,14 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-const foto: Array<string> = [
-	'satu.jpg',
-	'dua.jpg',
-	'tiga.jpg',
-	'empat.jpg',
-	'lima.jpg',
-	'enam.jpg',
-	'tujuh.jpg',
-];
+const foto: Array<string> = ['satu.jpg', 'dua.jpg', 'tiga.jpg'];
 const Galleri = () => {
 	return (
 		<section className='w-screen p-10 flex flex-wrap justify-center items-center gap-2'>
@@ -18,7 +10,7 @@ const Galleri = () => {
 					alt='wedding'
 					src={`/galeri/${val}`}
 					width={250}
-					height={250}
+					height={100}
 				/>
 			))}
 		</section>
